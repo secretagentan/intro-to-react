e2942d0742b9a88b082cb048abcc50596123e513
an nguyen <hello.an.ng@gmail.com>
2017-02-21 12:30:35 -0800

    renders nametags

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 console.log('✨')
-
 // () => window.alert('hi');
+
+class NameTag extends React.Component {
+
+  render() {
+    const tagStyle = {
+      backgroundColor: this.props.color,
+      height: '240px',
+      width: '360px',
+      textAlign: 'center',
+      color: 'white',
+      borderRadius: '14px'
+    }
+    const helloStyle = {
+      margin: '12px 0 0 0',
+      paddingTop: '4px'
+    }
+    const myNameIsStyle = {
+      margin: '0',
+      paddingBottom: '10px'
+    }
+    const nameFieldStyle = {
+      backgroundColor: 'white',
+      height: '58%',
+      fontSize: '40px',
+      color: 'black',
+      paddingTop: '20px'
+    }
+    return(
+      <div style={tagStyle}>
+        <h1 style={helloStyle}>H E L L O</h1>
+        <p style={myNameIsStyle}>my name is</p>
+        <div style={nameFieldStyle} id="name-field">{this.props.name}</div>
+      </div>
+    )
+  }
+}
 
 // create a SuperButton Component
 // color sets the background color of the Component
@@ -12,10 +47,11 @@ class SuperButton extends React.Component {
   render() {
     const style = {
       backgroundColor: this.props.color,
-      borderRadius: 4
+      borderRadius: 6,
+      color: 'black'
     };
     return(
-      <button onClick={this.handleClick} style={style}>Hi</button>
+      <button onClick={this.handleClick} style={style}>Hello</button>
     )
   }
 }
@@ -29,7 +65,7 @@ class Welcome extends React.Component {
 
   render() {
     return (
-      <div onDoubleClick={this.handleDoubleClick.bind(this)} style={{color: 'turquoise'}}>
+      <div onDoubleClick={this.handleDoubleClick.bind(this)} style={{color: 'white'}}>
         <p>Hello {this.props.adjective} <span>{this.props.name}</span></p>
       </div>
     )
@@ -40,13 +76,21 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello Avengers</h1>
         <Welcome adjective="pretty" name="Thor" />
         <Welcome adjective="hulky" name="Hulk" />
         <Welcome adjective="killer" name="Black Widow" />
-        <SuperButton color="hotpink" />
+        <SuperButton color="yellow" />
+
         <p>Playing with React</p>
         <p>Curly braces get evaluated as JS: {2 + 2}</p>
+
+        <NameTag color="red" name="Iron Man" />
+        <NameTag color="yellow" name="Thor" />
+        <NameTag color="orange" name="Black Widow" />
+        <NameTag color="green" name="Hulk" />
+        <NameTag color="blue" name="Captain America" />
+        <NameTag color="purple" name="Hawkeye" />
       </div>
     )
   }
